16ccb28c93f1189d8515b87e4572050758079d07
suzuran works <150839697+suzuran-works@users.noreply.github.com>
2024-08-21 19:00:47 +0900

    format

--- src/scripts00flask/summaryScene.ts
+++ src/scripts00flask/summaryScene.ts
@@ -13,94 +13,94 @@ import {preloadJson} from "../utility/preloadUtility.ts";
  * SummaryScene
  */
 export class SummaryScene extends Phaser.Scene {
-    
-    // シーンキー
-    public static Key = 'SummaryScene';
-    
-    
-    private flaskView?: FlaskView;
-    
-    /**
-     * コンストラクタ
+
+  // シーンキー
+  public static Key = 'SummaryScene';
+
+
+  private flaskView?: FlaskView;
+
+  /**
+   * コンストラクタ
+   */
+  constructor() {
+    super(SummaryScene.Key);
+    console.log('SummaryScene constructor');
+  }
+
+  /**
+   * プリロード
+   */
+  preload() {
+    console.log('SummaryScene preload');
+
+    // フラスコ用Jsonロード
+    preloadJson(this, PATH_JSONS.FLASK_LEFT_OUTLINE_A);
+  }
+
+  /**
+   * ゲームオブジェクト初期化
+   */
+  create() {
+    console.log('SummaryScene create');
+    this.createAsync().then();
+  }
+
+  private async createAsync() {
+    const canvas = this.game.canvas;
+
+    // URLからパラメータを取得
+    const params = new URLSearchParams(window.location.search);
+    const idx = params.get('idx');
+
+    // パラメータに基づいてゲームの設定を行う
+    let msg = 'flask';
+    if (idx) {
+      console.log(`index: ${idx}`);
+      msg = `フラスコの中の何か: ${idx}`;
+    }
+
+    // ビュー追加
+    await this.addViewAsync();
+
+    // テキスト
+    const text = this.add.text(0, 0, msg, {
+      fontSize: 30,
+      fontFamily: FONT_SMARTFONTUI,
+    });
+    text.setOrigin(0.5, 0.5);
+    text.setFill(GetColorCodeTextByRGB(180, 180, 180));
+    text.setPosition(canvas.width/2, canvas.height * 0.95);
+  }
+
+  private async addViewAsync() {
+    // シェーダーをロード
+    await loadSingleShaderTextAsync(this, SHADER_FOLDER, CATEGORY, 0);
+
+    // フラスコ
+    const canvas = this.game.canvas;
+    const width = canvas.width;
+    const height = canvas.height;
+    const shaderKey = getShaderKey(CATEGORY,0);
+    const jsonKey = getAssetResourceKey(PATH_JSONS.FLASK_LEFT_OUTLINE_A);
+    this.flaskView = new FlaskView(this, width, height, shaderKey, jsonKey);
+    this.flaskView.setPosition(canvas.width/2, canvas.height/2);
+
+    /*
+    tweenAsync(this, {
+        targets: this.flaskView,
+        duration: 3000,
+        y: 540,
+        repeat: -1,
+        yoyo: true,
+        ease: 'Sine.easeInOut'
+    }).then();
      */
-    constructor() {
-        super(SummaryScene.Key);
-        console.log('SummaryScene constructor');
-    }
-    
-    /**
-     * プリロード
-     */
-    preload() {
-        console.log('SummaryScene preload');
-        
-        // フラスコ用Jsonロード
-        preloadJson(this, PATH_JSONS.FLASK_LEFT_OUTLINE_A);
-    }
-    
-    /**
-     * ゲームオブジェクト初期化
-     */
-    create() {
-        console.log('SummaryScene create');
-        this.createAsync().then();
-    }
-    
-    private async createAsync() {
-        const canvas = this.game.canvas;
+  }
 
-        // URLからパラメータを取得
-        const params = new URLSearchParams(window.location.search);
-        const idx = params.get('idx');
-
-        // パラメータに基づいてゲームの設定を行う
-        let msg = 'flask';
-        if (idx) {
-            console.log(`index: ${idx}`);
-            msg = `フラスコの中の何か: ${idx}`;
-        }
-        
-        // ビュー追加
-        await this.addViewAsync();
-
-        // テキスト
-        const text = this.add.text(0, 0, msg, {
-            fontSize: 30,
-            fontFamily: FONT_SMARTFONTUI,
-        });
-        text.setOrigin(0.5, 0.5);
-        text.setFill(GetColorCodeTextByRGB(180, 180, 180));
-        text.setPosition(canvas.width/2, canvas.height * 0.95);
-    }
-    
-    private async addViewAsync() {
-        // シェーダーをロード
-        await loadSingleShaderTextAsync(this, SHADER_FOLDER, CATEGORY, 0);
-        
-        // フラスコ
-        const canvas = this.game.canvas;
-        const width = canvas.width;
-        const height = canvas.height;
-        const shaderKey = getShaderKey(CATEGORY,0);
-        const jsonKey = getAssetResourceKey(PATH_JSONS.FLASK_LEFT_OUTLINE_A);
-        this.flaskView = new FlaskView(this, width, height, shaderKey, jsonKey);
-        this.flaskView.setPosition(canvas.width/2, canvas.height/2);
-
-        /*
-        tweenAsync(this, {
-            targets: this.flaskView,
-            duration: 3000,
-            y: 540,
-            repeat: -1,
-            yoyo: true,
-            ease: 'Sine.easeInOut'
-        }).then();
-         */
-    }
-    
-    update() {
-        this.flaskView?.updateView();
-    }
+  update() {
+    this.flaskView?.updateView();
+  }
 }
 
 new Phaser.Game(createConfig([SummaryScene, AssetLoader]));
